7b54f10e3ff7d4e4fbec1cd2ac726d7023f94b22
Timothy Luedtke <timothyluedtke@gmail.com>
2023-02-05 14:09:27 -0600

    created grid layout for photos

--- src/screens/Photos.js
+++ src/screens/Photos.js
@@ -1,6 +1,9 @@
-import { Button, View, Text, StyleSheet, Image } from 'react-native';
-import React, { Flatlist, useState, useEffect } from 'react';
+import { Button, Dimensions, SafeAreaView, Text, StyleSheet, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
 import * as ImagePicker from 'expo-image-picker';
+import { FlatGrid } from 'react-native-super-grid';
+
+const { width } = Dimensions.get('window');
 
 export default function PhotosScreen() {
     const [image, setImage] = useState(null);
@@ -28,17 +31,34 @@ export default function PhotosScreen() {
         console.log(result);
 
         if (!result.canceled) {
-            setImage(result.assets[0].uri);
-            setExif(result.assets[0].exif);
+            setImage(result.assets.map((item) => item.uri));
+            setExif(result.assets.map((item) => item.exif));
+            console.log(result.assets.map((item) => item.exif));
+        } else {
+            alert('You have not selected any image');
         }
     };
 
     return (
-        <View style={styles.container}>
-            <Button title="Pick an image from camera roll" onPress={pickImage} />
-            {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+        <SafeAreaView style={styles.container}>
+            {image != null ? (
+                <FlatGrid
+                    itemDimension={width / 2}
+                    data={image}
+                    style={styles.gridView}
+                    spacing={0}
+                    renderItem={({ item }) => (
+                        <Image
+                            source={{ uri: item }}
+                            style={styles.image}
+                        />
+                    )}
+                />
+            ) : (
+                <Button title="Pick an image from camera roll" onPress={pickImage} />
+            )}
             {/* {exif && <Text>{JSON.stringify(exif)}</Text>} */}
-        </View>
+        </SafeAreaView>
     );
 }
 
@@ -47,5 +67,14 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    gridView: {
+        // marginTop: 20,
+        flex: 1,
+    },
+    image: {
+        resizeMode: 'cover',
+        width: width / 2,
+        height: width / 2,
     },
 });
